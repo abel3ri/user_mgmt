162a616a60a5ce15f99c97880970b956a7237aa4
abel3ri <abelmerete@yahoo.com>
2024-06-30 16:25:54 +0300

    modify: utilize router

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("./userModel");
+const router = require("./userRoutes");
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 (async () => {
   try {
@@ -16,38 +13,10 @@ app.use(express.urlencoded({ extended: true }));
   }
 })();
 
-app.get("/users", async (req, res) => {
-  const users = await User.find({});
-  res.status(200).json({ status: "success", data: users });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/users", async (req, res) => {
-  const { name, age } = req.body;
-  const user = new User({ name, age });
-  await user.save();
-  res
-    .status(200)
-    .json({ status: "success", message: "successfuly added a user." });
-});
-
-app.patch("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const { name, age } = req.body;
-  const updatedUser = await User.findByIdAndUpdate(
-    id,
-    { name, age },
-    { returnDocument: "after" }
-  );
-  res.status(200).json({ status: "success", data: updatedUser });
-});
-
-app.delete("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  await User.findByIdAndDelete(id);
-  res
-    .status(200)
-    .json({ status: "success", message: "successfuly deleted a user." });
-});
+app.use("/api/v1", router);
 
 app.use("*", (req, res) => {
   res.status(404).json({ status: "fail", message: "route not found." });
---
+++ userRoutes.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const User = require("./userModel");
+
+router
+  .route("/users")
+  .get(async (req, res) => {
+    const users = await User.find({});
+    res.status(200).json({ status: "success", data: users });
+  })
+  .post(async (req, res) => {
+    const { name, age } = req.body;
+    const user = new User({ name, age });
+    await user.save();
+    res
+      .status(200)
+      .json({ status: "success", message: "successfuly added a user." });
+  });
+
+router
+  .route("/users/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json({ status: "success", data: user });
+  })
+  .patch(async (req, res) => {
+    const { id } = req.params;
+    const { name, age } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { name, age },
+      { returnDocument: "after" }
+    );
+    res.status(200).json({ status: "success", data: updatedUser });
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ status: "success", message: "successfuly deleted a user." });
+  });
+
+module.exports = router;
